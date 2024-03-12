1137d58fcf130d83f9aeded66e9ac4bda331528a
sieun <limsieun0313@gmail.com>
2024-03-12 19:59:13 +0900

    refactor: 헤더 Menu data 분리

---
+++ src/data/menu.ts
@@ -0,0 +1,19 @@
+type Menu = {
+  href: string;
+  tab: string;
+};
+
+export const MENUS: Menu[] = [
+  {
+    href: '/about',
+    tab: 'About',
+  },
+  {
+    href: '/project',
+    tab: 'Project',
+  },
+  {
+    href: '/study',
+    tab: 'Study',
+  },
+];
