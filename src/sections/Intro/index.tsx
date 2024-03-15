40ff5646cc0b2a56e31ac1d2d568e417c38102db
sieun <limsieun0313@gmail.com>
2024-03-15 14:02:26 +0900

    fix: gap Props 선택자옵션으로 변경

--- src/sections/Intro/index.tsx
+++ src/sections/Intro/index.tsx
@@ -1,23 +1,14 @@
 'use client';
 
+import LayoutContainer from '@/components/LayoutContainer';
 import ScrollDown from '@/components/scroll/ScrollDown';
 import IntroContentText from './IntroContentText';
 
 export default function Intro() {
   return (
-    <section
-      style={{
-        width: '100%',
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        background: '#f2f2f2',
-      }}
-    >
+    <LayoutContainer background="#f2f2f2">
       <IntroContentText />
       <ScrollDown />
-    </section>
+    </LayoutContainer>
   );
 }
