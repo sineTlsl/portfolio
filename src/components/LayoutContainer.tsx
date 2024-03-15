2be07736aa45a2a890dc4a75baab3d27580f2b8b
sieun <limsieun0313@gmail.com>
2024-03-15 13:58:40 +0900

    feat: 섹션 공통 컴포넌트 구현

---
+++ src/components/LayoutContainer.tsx
@@ -0,0 +1,34 @@
+type flexDirection = 'row' | 'row-reverse' | 'column' | 'column-reverse';
+
+type Props = {
+  height?: string;
+  flexDirection?: flexDirection;
+  background: string;
+  gap: string;
+  children?: React.ReactNode;
+};
+
+export default function LayoutContainer({
+  height = '100vh',
+  flexDirection = 'column',
+  background,
+  gap = '0rem',
+  children,
+}: Props) {
+  return (
+    <section
+      style={{
+        width: '100%',
+        height: height,
+        display: 'flex',
+        flexDirection: flexDirection,
+        justifyContent: 'center',
+        alignItems: 'center',
+        background: background,
+        gap: gap,
+      }}
+    >
+      {children}
+    </section>
+  );
+}
