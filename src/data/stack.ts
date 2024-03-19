b7889c5c10c2ce543f875de2cdfb2c9dc7fc9963
sieun <limsieun0313@gmail.com>
2024-03-19 17:24:49 +0900

    data: 기술 스택 아이콘 이미지 매핑 데이터

---
+++ src/data/stack.ts
@@ -0,0 +1,35 @@
+type TechStack = {
+  icon: string;
+  image: string;
+};
+
+export const TEACHSTACK: TechStack[] = [
+  {
+    icon: 'React',
+    image: 'react.webp',
+  },
+  {
+    icon: 'Typescript',
+    image: 'typescript.png',
+  },
+  {
+    icon: 'Nextjs',
+    image: 'nextjs.png',
+  },
+  {
+    icon: 'React Query',
+    image: 'react-query.png',
+  },
+  {
+    icon: 'Styled Components',
+    image: 'styled-components.png',
+  },
+  {
+    icon: 'Zustand',
+    image: 'zustand.png',
+  },
+  {
+    icon: 'Redux Toolkit',
+    image: 'redux-toolkit.webp',
+  },
+];
