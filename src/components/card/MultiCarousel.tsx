abb0274892ecd6014e8d4eb3b3d3db3f05f04e58
sieun <limsieun0313@gmail.com>
2024-03-29 00:55:50 +0900

    feat: 프로젝트 멀티 캐러쉘 적용

---
+++ src/components/card/MultiCarousel.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { ReactNode } from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 1025 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 767, min: 0 },
+    items: 1,
+  },
+};
+
+export default function MultiCarousel({ children }: { children: ReactNode }) {
+  return (
+    <Carousel infinite autoPlay responsive={responsive}>
+      {children}
+    </Carousel>
+  );
+}
--- src/sections/Project/ProjectCardList.tsx
+++ src/sections/Project/ProjectCardList.tsx
@@ -1,3 +1,4 @@
+import MultiCarousel from '@/components/card/MultiCarousel';
 import ProjectCard from '@/components/card/ProjectCard';
 import { getAllProjects } from '@/service/projects';
 
@@ -5,10 +6,12 @@ export default async function ProjectCardList() {
   const projects = await getAllProjects();
 
   return (
-    <div style={{ display: 'flex', gap: '1.5rem' }}>
-      {projects.map((project) => (
-        <ProjectCard key={project.name} project={project} />
-      ))}
+    <div style={{ width: '100%', maxWidth: '1100px', margin: '0 1rem' }}>
+      <MultiCarousel>
+        {projects.map((project) => (
+          <ProjectCard key={project.name} project={project} />
+        ))}
+      </MultiCarousel>
     </div>
   );
 }
