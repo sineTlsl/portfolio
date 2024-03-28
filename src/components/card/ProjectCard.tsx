376c42cce5d6d183e6290eb5dc114946f5187750
sieun <limsieun0313@gmail.com>
2024-03-28 18:43:17 +0900

    fix: 이미지 유틸 함수와 경로 변경

--- src/components/card/ProjectCard.tsx
+++ src/components/card/ProjectCard.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import styles from './css/ProjectCard.module.css';
 import { Project } from '@/types/project';
 import CardText from './CardText';
+import Link from 'next/link';
+import { isGif } from '@/utils/image';
 
 type Props = {
   project: Project;
@@ -9,20 +11,23 @@ type Props = {
 
 export default function ProjectCard({ project }: Props) {
   return (
-    <div className={styles['card-wrap']}>
-      <div className={styles['preview-img']}>
-        <Image
-          src={`/assets/images/project/${project.image}`}
-          fill
-          style={{
-            objectFit: 'cover',
-          }}
-          alt="preview"
-          sizes="100%"
-          priority
-        />
+    <Link href={`projects/${project.path}`}>
+      <div className={styles['card-wrap']}>
+        <div className={styles['preview-img']}>
+          <Image
+            src={`/assets/images/project/${project.image}`}
+            fill
+            style={{
+              objectFit: 'cover',
+            }}
+            alt="preview"
+            sizes="100%"
+            unoptimized={isGif(project.image)}
+            priority={isGif(project.image)}
+          />
+        </div>
+        <CardText project={project} />
       </div>
-      <CardText project={project} />
-    </div>
+    </Link>
   );
 }
