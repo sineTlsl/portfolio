9572b6dd20316ec8184ac10458b138adde5fb006
sieun <limsieun0313@gmail.com>
2024-03-31 14:37:53 +0900

    style: 아이콘 props 스타일 변경

--- src/components/ui/icons/BlogFillIcon.tsx
+++ src/components/ui/icons/BlogFillIcon.tsx
@@ -1,5 +1,13 @@
 import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 
-export default function BlogFillIcon() {
-  return <AutoStoriesIcon sx={{ fontSize: 30 }} color='action' />;
+type Props = {
+  fontSize?: number;
+  color?: string;
+};
+
+export default function BlogFillIcon({
+  fontSize = 30,
+  color = 'rgba(0, 0, 0, 0.54)',
+}: Props) {
+  return <AutoStoriesIcon sx={{ fontSize, color }} />;
 }
--- src/components/ui/icons/GithubFillIcon.tsx
+++ src/components/ui/icons/GithubFillIcon.tsx
@@ -1,5 +1,13 @@
 import GitHubIcon from '@mui/icons-material/GitHub';
 
-export default function GithubFillIcon() {
-  return <GitHubIcon sx={{ fontSize: 30 }} color='action' />;
+type Props = {
+  fontSize?: number;
+  color?: string;
+};
+
+export default function GithubFillIcon({
+  fontSize = 30,
+  color = 'rgba(0, 0, 0, 0.54)',
+}: Props) {
+  return <GitHubIcon sx={{ fontSize, color }} />;
 }
