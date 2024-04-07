9333c1974eb66529ba34062226f90519c17da174
sieun <limsieun0313@gmail.com>
2024-04-07 23:08:27 +0900

    feat: 형광펜 애니메이션 효과 적용

--- src/sections/skill/SubText.tsx
+++ src/sections/skill/SubText.tsx
@@ -1,16 +1,22 @@
+'use client';
+
 import React from 'react';
 import { SUBTEXTS } from '@/constants/intro';
 import styles from './css/SubText.module.css';
+import useScrollAnimation from '@/hooks/useScrollAnimation';
 
 const pointText = ['원활한 소통', '긍정적인 사고방식', '성공적인 결과'];
 
-function highlightText(text: string) {
+function highlightText(text: string, isVisible: boolean) {
   const regex = new RegExp(`(${pointText.join('|')})`, 'g');
   const parts = text.split(regex);
 
   return parts.map((part, idx) =>
     pointText.includes(part) ? (
-      <span key={idx} className={styles.bold}>
+      <span
+        key={idx}
+        className={`${styles.bold} ${isVisible ? styles.highlight : ''}`}
+      >
         {part}
       </span>
     ) : (
@@ -20,12 +26,14 @@ function highlightText(text: string) {
 }
 
 export default function SubText() {
+  const { isVisible, animationRef } = useScrollAnimation<HTMLUListElement>();
+
   return (
-    <ul className={styles['flex-style']}>
+    <ul className={styles['flex-style']} ref={animationRef}>
       {SUBTEXTS.map((text, textIdx) => (
         <li key={textIdx} className={styles.text}>
           {text.split(',').map((s, idx) => (
-            <p key={idx}>{highlightText(s)}</p>
+            <p key={idx}>{highlightText(s, isVisible)}</p>
           ))}
         </li>
       ))}
